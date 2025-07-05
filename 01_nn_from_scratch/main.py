a9b8ba7659b3aa220bcae9f3dc814363d1961d3f
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-05 17:12:08 +0000

    hyperparameters

--- 01_nn_from_scratch/main.py
+++ 01_nn_from_scratch/main.py
@@ -3,3 +3,9 @@ from utils import sigmoid, sigmoid_derivative, mean_squared_error, mse_derivativ
 
 X = np.array([[0,0], [0,1], [1,0], [1,1]])
 y = np.array([[0], [1], [1], [0]])
+
+input_size = 2
+hidden_size = 2
+output_size = 1
+learning_rate = 0.1
+epochs = 10000
