4d2c2be3aad7af062885c3b000318d8983cdf040
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-05 17:11:04 +0000

    input

--- 01_nn_from_scratch/main.py
+++ 01_nn_from_scratch/main.py
@@ -1,2 +1,5 @@
 import numpy as np
 from utils import sigmoid, sigmoid_derivative, mean_squared_error, mse_derivative
+
+X = np.array([[0,0], [0,1], [1,0], [1,1]])
+y = np.array([[0], [1], [1], [0]])
