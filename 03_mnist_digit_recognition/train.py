825f67eef0c0f94d4d4f25b65d87e9ed7a170e91
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-07 17:07:59 +0000

    packages

---
+++ 03_mnist_digit_recognition/train.py
@@ -0,0 +1,8 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
