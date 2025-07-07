fb9a2ff1114af843ab64d8186b0c8b6ffa3b236b
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-07 17:17:20 +0000

    model

--- 03_mnist_digit_recognition/model.py
+++ 03_mnist_digit_recognition/model.py
@@ -11,3 +11,10 @@ model = load_model('mlp_mnist.h5')
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
+
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
