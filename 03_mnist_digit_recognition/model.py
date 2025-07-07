4da58734eda5f7902eb4d3b14571ea0af0a3b7e9
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-07 17:17:46 +0000

    models

--- 03_mnist_digit_recognition/model.py
+++ 03_mnist_digit_recognition/model.py
@@ -18,3 +18,6 @@ model = Sequential([
     Dense(64, activation='relu'),
     Dense(10, activation='softmax')
 ])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.load_weights('mlp_weights.h5')
