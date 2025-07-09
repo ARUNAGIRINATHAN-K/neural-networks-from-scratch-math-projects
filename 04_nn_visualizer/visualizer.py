06c4de8697df5c479fd20759ead1c6f06af235c7
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-09 12:38:28 +0000

    sigmoid

--- 04_nn_visualizer/visualizer.py
+++ 04_nn_visualizer/visualizer.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 # Sigmoid activation and derivative
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    s = sigmoid(x)
+    return s * (1 - s)
