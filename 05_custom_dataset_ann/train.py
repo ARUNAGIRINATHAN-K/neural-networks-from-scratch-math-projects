2306b79dc688dff76dd22d8799222fa03d97fc88
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-10 16:19:53 +0000

    Train-test

--- 05_custom_dataset_ann/train.py
+++ 05_custom_dataset_ann/train.py
@@ -19,3 +19,6 @@ y_encoded = label_encoder.fit_transform(y)
 # Normalize features
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_encoded, test_size=0.2, random_state=42)
