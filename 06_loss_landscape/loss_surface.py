e9e1d0ffacfcd22a42593bae292995448e7b10fb
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-12 17:33:35 +0000

    Toy-dataset

--- 06_loss_landscape/loss_surface.py
+++ 06_loss_landscape/loss_surface.py
@@ -1,3 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+# Toy-data
+np.random.seed(42)
+X = np.linspace(0, 1, 100).reshape(-1, 1)
+true_w, true_b = 2, 3
+y = true_w * X + true_b + np.random.normal(0, 0.1, size=X.shape)
