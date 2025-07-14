98d2ac6656d5e3efc72591e2e1326651dbcbc453
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-14 17:45:31 +0000

    activation

--- 08_activation_function_analysis/compare_activations.py
+++ 08_activation_function_analysis/compare_activations.py
@@ -9,3 +9,6 @@ from sklearn.neural_network import MLPClassifier
 X, y = make_moons(n_samples=1000, noise=0.2, random_state=42)
 X = StandardScaler().fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+activations = ['logistic', 'tanh', 'relu']
+histories = {}
