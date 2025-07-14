13def5b71150fef3cab6842aba1498e8c1994caf
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-14 17:45:01 +0000

    train

--- 08_activation_function_analysis/compare_activations.py
+++ 08_activation_function_analysis/compare_activations.py
@@ -8,3 +8,4 @@ from sklearn.neural_network import MLPClassifier
 #sample-dataset
 X, y = make_moons(n_samples=1000, noise=0.2, random_state=42)
 X = StandardScaler().fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
