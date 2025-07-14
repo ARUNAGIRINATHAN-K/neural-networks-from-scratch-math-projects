aee491ddb577394f3020ff4acc2eb1816d82f8e9
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-07-14 17:43:46 +0000

    sample-dataset's

--- 08_activation_function_analysis/compare_activations.py
+++ 08_activation_function_analysis/compare_activations.py
@@ -4,3 +4,5 @@ from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
+
+#sample-dataset
